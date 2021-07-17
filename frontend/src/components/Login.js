01b57e404f863fdce9eb11cd8566965110a4e4fb
kphan20 <knpmths@gmail.com>
2021-07-16 21:50:22 -0400

    Added registration page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -7,14 +7,19 @@ import axios from "axios";
 function Login(props) {
   const [userName, setUserName] = useState("");
   const [password, setPassWord] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
   const history = useHistory();
+  const [errorMessage, setError] = useState("");
+  const changePage = () => {
+    setIsLogin(!isLogin);
+  };
   const handleChange = (changeFunction) => (e) => {
     changeFunction(e.target.value);
   };
   const handleSubmit = (e) => {
     e.preventDefault();
     axios
-      .post(apiLink + "api-token-auth/", {
+      .post(apiLink + (isLogin ? "api-token-auth/" : "register/"), {
         username: userName,
         password: password,
       })
@@ -25,14 +30,33 @@ function Login(props) {
           history.push("/");
         },
         (error) => {
-          console.log(error);
+          //console.log(error);
+          setError("Invalid username/password");
         }
       );
   };
+  // const handleRegistration = (e) => {
+  //   e.preventDefault();
+  //   axios
+  //     .post(apiLink + "register/", {
+  //       username: userName,
+  //       password: password,
+  //     })
+  //     .then(
+  //       (response) => {
+  //         console.log(response);
+  //         props.setAuthInfo(response.data);
+  //         history.push("/");
+  //       },
+  //       (error) => {
+  //         console.log(error);
+  //       }
+  //     );
+  // };
   return (
     <div id="screen">
       <div id="loginbox">
-        <p>Login screen</p>
+        <p>{isLogin ? "Login screen" : "Register screen"}</p>
         <Link to="/">
           <p>x</p>
         </Link>
@@ -55,6 +79,10 @@ function Login(props) {
           </label>
           <input type="submit" value="Submit" />
         </form>
+        <p id="error">{errorMessage}</p>
+        <button onClick={changePage}>
+          {isLogin ? "Register" : "Back to login"}
+        </button>
       </div>
     </div>
   );
